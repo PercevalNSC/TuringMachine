7716a4553030971db7ff6f4c2bd50832cdfe759e
PercevalNSC <wait-keita1998@outlook.jp>
2022-05-13 16:31:26 +0900

    add node & wordPath

--- dtm/dtm.go
+++ dtm/dtm.go
@@ -2,43 +2,127 @@ package dtm
 
 type TuringMachine struct {
 	words          string
-	void_character byte
+	void_character string
 }
 
 func TuringMachineSet(w string, v_c byte) TuringMachine {
 	var turing_machine TuringMachine
 	turing_machine.words = w
-	turing_machine.void_character = v_c
+	turing_machine.void_character = string(v_c)
 	return turing_machine
 }
 func (tm *TuringMachine) overWrite(b byte, i int) {
 	tm.words = OverWrite(tm.words, b, i)
 }
-func (tm *TuringMachine) rootingNode(status_id int, position int) int {
-	switch status_id {
-	case 1:
-		return tm.p0Node(position)
-	case 2:
-		return tm.paNode(position)
-	default:
-		return -1
-	}
+
+type TuringPath struct {
+	goto_id      int
+	replace_char string
+	head_moveto  int
 }
 
-func (tm *TuringMachine) p0Node(i int) int {
-	// id: 1
-	if tm.words[i] == 'a' {
-		tm.overWrite('o', i)
-		return 2 // to pa
-	} else if tm.words[i] == 'b' {
-		tm.overWrite('p', i)
-		return 3 // to pb
-	} else {
-		return -1
-	}
+func errorTuringPath() TuringPath {
+	return TuringPath{0, "", 0}
 }
-func (tm *TuringMachine) paNode(i int) int {
-	return -1
+
+type TuringNode struct {
+	id    int
+	route map[string]TuringPath
+}
+
+func (tn *TuringNode) wordPath(input_word string) TuringPath {
+	path, status := tn.route[input_word]
+	if status {
+		return path
+	} else {
+		return errorTuringPath()
+	}
+
+}
+
+func (tm *TuringMachine) p0Node() TuringNode {
+	id := 1
+	path1 := TuringPath{2, "o", 1}
+	path2 := TuringPath{3, "p", 1}
+	path3 := TuringPath{7, "#", -1}
+	route := map[string]TuringPath{"a": path1, "b": path2, "#": path3}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) paNode() TuringNode {
+	id := 2
+	path1 := TuringPath{2, "a", 1}
+	path2 := TuringPath{2, "b", 1}
+	path3 := TuringPath{2, "#", 1}
+	path4 := TuringPath{4, "a", 1}
+	route := map[string]TuringPath{
+		"a":               path1,
+		"b":               path2,
+		"#":               path3,
+		tm.void_character: path4,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) pbNode() TuringNode {
+	id := 3
+	path_a := TuringPath{3, "a", 1}
+	path_b := TuringPath{3, "b", 1}
+	path_sh := TuringPath{3, "#", 1}
+	path_void := TuringPath{5, "b", 1}
+	route := map[string]TuringPath{
+		"a":               path_a,
+		"b":               path_b,
+		"#":               path_sh,
+		tm.void_character: path_void,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) padNode() TuringNode {
+	id := 4
+	path_void := TuringPath{6, "b", -1}
+	route := map[string]TuringPath{
+		tm.void_character: path_void,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) pbdNode() TuringNode {
+	id := 5
+	path_void := TuringPath{6, "a", -1}
+	route := map[string]TuringPath{
+		tm.void_character: path_void,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) lNode() TuringNode {
+	id := 6
+	path_a := TuringPath{6, "a", -1}
+	path_b := TuringPath{6, "b", -1}
+	path_sh := TuringPath{6, "#", -1}
+	path_o := TuringPath{1, "o", 1}
+	path_p := TuringPath{1, "p", 1}
+	route := map[string]TuringPath{
+		"a": path_a,
+		"b": path_b,
+		"#": path_sh,
+		"o": path_o,
+		"p": path_p,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) cNode() TuringNode {
+	id := 7
+	path_o := TuringPath{7, "a", -1}
+	path_p := TuringPath{7, "b", -1}
+	path_void := TuringPath{8, tm.void_character, 1}
+	route := map[string]TuringPath{
+		"o":               path_o,
+		"p":               path_p,
+		tm.void_character: path_void,
+	}
+	return TuringNode{id, route}
+}
+func (tm *TuringMachine) fNode() TuringNode {
+	id := 8
+	return TuringNode{id, nil}
 }
 
 func OverWrite(w string, b byte, i int) string {
--- dtm/dtm_test.go
+++ dtm/dtm_test.go
@@ -1,6 +1,8 @@
 package dtm
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -32,5 +34,73 @@ func TestTuringMachine(t *testing.T) {
 		if w2 != tm.words {
 			t.Error(tm.words)
 		}
+	})
+	t.Run("p0Node", func(t *testing.T) {
+		node := tm.p0Node()
+		if node.id != 1 {
+			t.Error(node)
+		}
+		if !reflect.DeepEqual(node.wordPath("a"), TuringPath{2, "o", 1}) {
+			t.Error(node.wordPath("a"))
+		}
+		if !reflect.DeepEqual(node.wordPath("b"), TuringPath{3, "p", 1}) {
+			t.Error(node.wordPath("b"))
+		}
+		if !reflect.DeepEqual(node.wordPath("#"), TuringPath{7, "#", -1}) {
+			t.Error(node.wordPath("#"))
+		}
+		if !reflect.DeepEqual(node.wordPath("x"), errorTuringPath()) {
+			t.Error(node.wordPath("x"))
+		}
+		fmt.Println(node)
+	})
+	t.Run("paNode", func(t *testing.T) {
+		node := tm.paNode()
+		if node.id != 2 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("pbNode", func(t *testing.T) {
+		node := tm.pbNode()
+		if node.id != 3 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("padNode", func(t *testing.T) {
+		node := tm.padNode()
+		if node.id != 4 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("pbdNode", func(t *testing.T) {
+		node := tm.pbdNode()
+		if node.id != 5 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("lNode", func(t *testing.T) {
+		node := tm.lNode()
+		if node.id != 6 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("cNode", func(t *testing.T) {
+		node := tm.cNode()
+		if node.id != 7 {
+			t.Error(node)
+		}
+		fmt.Println(node)
+	})
+	t.Run("fNode", func(t *testing.T) {
+		node := tm.fNode()
+		if node.id != 8 {
+			t.Error(node)
+		}
+		fmt.Println(node)
 	})
 }
